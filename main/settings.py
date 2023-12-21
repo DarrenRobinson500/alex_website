853673a3d0bad17ca25a6c58da79dcc3fd65e0c3
Alex Robinson <darrenandamanda.robinson@gmail.com>
2023-12-21 17:44:16 +1100

    Added AWS RDS

--- main/settings.py
+++ main/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backendcourse',
+        'USER': 'mysuperuser',
+        'PASSWORD': 'mysuperuser',
+        'HOST': 'backendcourse.c10sm4osu84m.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
