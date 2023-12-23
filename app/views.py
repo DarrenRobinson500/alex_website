44a50f15421165028eb5dc0b112bb1ed5f387dd6
Darren <darrenandamanda.robinson@gmail.com>
2023-12-23 15:02:24 +1100

    Change Math to Maths

--- app/views.py
+++ app/views.py
@@ -3,6 +3,17 @@ from .models import *
 from .forms import *
 from django.core.mail import send_mail
 
+from twilio.rest import Client
+
+account_sid = 'ACa0c8a968a5cf31589723fc546a495ca0'
+auth_token = '7c2f462ef25881f44a9acecaf8143a80'
+my_twilio_number = '19282884569'
+daz = '+61493461541'
+alex = '+61491062546'
+
+client = Client(account_sid, auth_token)
+
+
 def home(request):
 
     quotes = Quote.objects.all
@@ -25,8 +36,9 @@ def booking(request):
         booking_subject = request.POST['subject']
         booking_message = request.POST['message']
 
-        # send_mail(booking_subject + ' ' + booking_name, booking_message, booking_email, ['darrenandamanda.robinson@gmail.com', ])
-
+        body = f"'{booking_name}' has requested a booking",
+        for to in [daz, alex]:
+            client.messages.create(body=body, from_=my_twilio_number, to=to)
 
     return redirect("home")
 
